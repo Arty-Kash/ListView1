de8d57878d27d559e62fe5f56e32549699020309
Arty Kash <33560795+Arty-Kash@users.noreply.github.com>
2017-12-24 09:30:05 +0900

    Add files via upload

---
+++ BookInfo.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+namespace ListView
+{
+    public partial class BookInfo : ContentPage
+    {
+        /* Book Information Page
+                showing the tapped book information */
+        public BookInfo(string[] TappedBook)
+        {
+            InitializeComponent();
+            Label1.Text = TappedBook[0];
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+namespace ListView
+{
+    public partial class MainPage : ContentPage
+    {
+        /* Create a book list data "books" as a dynamic data collection
+           that provides notifications when a book get added */
+        public static ObservableCollection<string> books { get; set; }
+        public MainPage()
+        {
+            /* Create a new book list instance "books"
+                            and set the initialize data */
+            books = new ObservableCollection<string>()
+            { 
+                "Killing Floor (Lee Child)", 
+                "Man On Fire (A.J.Quinnell)", 
+                "The Lion's Game (Nelson DeMille)"
+            };
+
+            InitializeComponent();
+            /* Bind the book list data "books" to
+                 the ListView control "BookListView" */
+            BookListView.ItemsSource = books;
+        }
+
+        /* Insert the new book to the book list */
+        void OnNewBook(object sender, EventArgs e)
+        {
+            books.Insert(0, Entry1.Text);
+        }
+
+        /* If one of the books was tapped, move to the Book Information
+           page "BookInfo" with the tapped book information */
+        async void OnTap(object sender, ItemTappedEventArgs e)
+        {
+            string[] TappedBook = { e.Item.ToString() };
+            await Navigation.PushAsync(new BookInfo(TappedBook));
+        }
+    }
+}
